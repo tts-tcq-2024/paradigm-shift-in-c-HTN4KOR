50043d4be2b9a21d35790668e8ace43be664324f
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-06-23 18:04:13 +0530

    Update checker.c
    Language Paradigm Commit 4

--- checker.c
+++ checker.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
-bool temperratureIsOk(float temperature)
-{
-  if(temperature < 0 || temperature > 45) {
+bool temperratureIsOk(float temperature){
+  if(temperature < 0 || temperature > 45){
     printf("Temperature out of range!\n");
     return 0;
   }
   return 1;
 }
-
-bool SocIsOk(float soc)
-{
+bool SocIsOk(float soc){
 if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
   }
   return 1;
 }
-bool ChargeRateIsOk(float chargeRate)
-{
+bool ChargeRateIsOk(float chargeRate){
 if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
