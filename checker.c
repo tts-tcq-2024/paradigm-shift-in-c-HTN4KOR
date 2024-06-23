153a6c94651fa42732a23f48da795e89124000b9
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-06-23 18:10:43 +0530

    Update checker.c
    Language paradigm Commit 6 complexity

--- checker.c
+++ checker.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
-#include <stdbool.h>
-bool temperratureIsOk(float temperature){
+//#include <stdbool.h>
+int temperratureIsOk(float temperature){
   if(temperature < 0 || temperature > 45){
     printf("Temperature out of range!\n");
     return 0;
   }
   return 1;
 }
-bool SocIsOk(float soc){
+int SocIsOk(float soc){
 if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
   }
   return 1;
 }
-bool ChargeRateIsOk(float chargeRate){
+int ChargeRateIsOk(float chargeRate){
 if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
@@ -23,7 +23,7 @@ if(chargeRate > 0.8) {
   return 1;
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
+int batteryIsOk(float temperature, float soc, float chargeRate) {
   return temperratureIsOk(temperature) && SocIsOk(soc) && ChargeRateIsOk(chargeRate);
 }
 int main() {
